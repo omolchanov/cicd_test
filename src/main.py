f41a487700833b8721690485522824ef8a91d9a5
Alex <oleksandr.molchanov@gmail.com>
2025-06-13 13:07:29 +0300

    Workflow - Added get_float func

--- src/main.py
+++ src/main.py
@@ -5,5 +5,10 @@ def get_random_int():
     return random.randint(1, 100)
 
 
+def get_random_float():
+    return random.random()
+
+
 if __name__ == '__main__':
     print(get_random_int())
+    print(get_random_float())
--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,15 @@
 import unittest
 
-from src.main import get_random_int
+from src.main import *
 
 
 class TestGetRandomFunction(unittest.TestCase):
+
     def test_get_random_int(self):
         self.assertIsInstance(get_random_int(), int)
+
+    def test_get_random_float(self):
+        self.assertIsInstance(get_random_float(), float)
 
 
 if __name__ == '__main__':
