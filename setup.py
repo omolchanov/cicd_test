c6639dd8074f2c49101011e16d8289ce7990dacb
Alex <oleksandr.molchanov@gmail.com>
2025-06-14 19:34:13 +0300

    Workflow - add uploading to pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CICDTest_omolchanov1',
+    name='CICDTest_omolchanov',
     version='0.1.1',
     license='MIT',
-    packages=find_packages(where='src'),
-    package_dir={'': ''},
+    packages=find_packages(),  # No 'where' needed
     description='Short description',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
-    install_requires=[
-        # List dependencies here, e.g., "numpy", "requests"
-    ],
-
+    install_requires=[],
     entry_points={
         'console_scripts': [
-            'cli=main:main',  # Optional command-line entry point
+            'cli=cicdtest_omolchanov1.main:main',
         ]
     },
 )
