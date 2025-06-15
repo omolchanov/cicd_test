0d9bcadf5d55576d4ed138651e2fe39035d0772f
Alex <oleksandr.molchanov@gmail.com>
2025-06-15 13:09:39 +0300

    Workflow - 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     py_modules=['main'],
     license='MIT',
     package_dir={'': 'src'},
-    packages=find_packages(),  # No 'where' needed
     description='Short description',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
