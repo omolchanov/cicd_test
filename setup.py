921d7f417ad4a17a961ed3763952a2f06dabae23
Alex <oleksandr.molchanov@gmail.com>
2025-06-15 13:19:25 +0300

    Workflow - 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CICDTest_omolchanov',
-    version='0.1.4',
+    version='0.1.5',
     py_modules=['main'],
     license='MIT',
     package_dir={'': 'src'},
@@ -12,7 +12,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'cicd = src.main:main',
+            'my_app_test = src.main:main',
         ]
     }
 )
