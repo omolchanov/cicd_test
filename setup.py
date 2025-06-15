fbba5b64726109e015b8d9b8bc34e48d832307b1
Alex <oleksandr.molchanov@gmail.com>
2025-06-15 12:55:55 +0300

    Workflow - add uploading to pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'cli=cicdtest_omolchanov.main:main',
+            'cicd=cicdtest_omolchanov.main:main',
         ]
     },
 )
