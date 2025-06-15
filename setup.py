ae88c5922e89aca41d7c2209ef4e2267647adaab
Alex <oleksandr.molchanov@gmail.com>
2025-06-15 19:17:58 +0300

    Workflow -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ci_cd_test_omolchanov',
-    version='0.1.8',
+    version='0.1.9',
     py_modules=['main'],
     license='MIT',
     packages=find_packages(where='src'),
